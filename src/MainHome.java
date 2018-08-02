e80a6fc161b5105cd466d6227d5eba8b997cee4a
LordPeaches <AlexHughesGMU@gmail.com>
2018-08-02 11:18:25 -0400

    Testing an extra method.

--- src/MainHome.java
+++ src/MainHome.java
@@ -3,6 +3,11 @@ public class MainHome {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
+		testmethod();
 	}
-
+	
+	public static void testmethod() {
+		System.out.print("Just a little test, ignore me");
+	}
 }
