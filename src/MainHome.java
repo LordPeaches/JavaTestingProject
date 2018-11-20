1c91425ff80b7faca0a9388e7c631ae205864056
LordPeaches <AlexHughesGMU@gmail.com>
2018-11-20 15:07:00 -0500

    Added method1() and method2()
    Added a few methods for testing purposes

--- src/MainHome.java
+++ src/MainHome.java
@@ -4,10 +4,21 @@ public class MainHome {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		
+		method1();
 		testmethod();
 	}
 	
 	public static void testmethod() {
-		System.out.print("Just a little test, ignore me");
+		System.out.println("Just a little test, ignore me");
+		System.out.println(method2() + " is your answer");
+	}
+	
+	public static void method1() {
+		System.out.println("This is method1");
+	}
+	
+	public static int method2() {
+		int x = 7 + 12;
+		return x;
 	}
 }
